07cd7ef411738a60d0ccdea998fc96a23bb57550
Ujwal-Shetty <ujwalshetty381@gmail.com>
2024-04-14 13:31:01 +0530

    Objects

---
+++ dist/objects.js
@@ -0,0 +1,4 @@
+"use strict";
+let employee1 = { id: 1, name: '' };
+employee1.name = 'ufrf';
+//# sourceMappingURL=objects.js.map
---
+++ src/objects.ts
@@ -0,0 +1,6 @@
+let employee1 :{
+    id:number,
+    name:string
+}= {id:1,name:''}
+
+employee1.name='ufrf'
